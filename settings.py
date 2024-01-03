df955b4f8da121264dee8daaad7bdea4278da1cd
Sengsathit <sengsathit@gmail.com>
2024-01-03 14:44:00 +0100

    Display ecosystem V1

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 class Settings:
-    ocean_width = 20
-    ocean_height = 20
+    ocean_width = 10
+    ocean_height = 10
     nb_fishes = (ocean_width * ocean_height) * 0.7
     nb_tunas = nb_fishes * 0.6
     nb_sharks = nb_fishes * 0.4
