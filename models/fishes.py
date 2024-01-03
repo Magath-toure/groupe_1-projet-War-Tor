0cccebd68b70fdba4e7d5b6f77d52ff001d8caa2
Serge Buasa <sergebuasa@gmail.com>
2024-01-03 11:02:26 +0100

    nouvelle version

---
+++ models/fishes.py
@@ -0,0 +1,19 @@
+from settings import Settings
+
+class Fish :
+    def __init__(self, position : tuple):
+        self.position = position
+    
+    def move (self):
+        pass
+
+    def reproduce (self):
+        pass
+
+
+class Tuna (Fish) :
+    pass
+
+
+class Shark (Fish):
+    energy = Settings.shark_energy
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 class Settings:
     ocean_width = 20
     ocean_height = 20
-    nb_max_fishes = (ocean_width * ocean_height) * 0.7
-    nb_max_tuna = nb_max_fishes * 0.6
-    nb_max_sharks = nb_max_fishes * 0.4
+    nb_fishes = (ocean_width * ocean_height) * 0.7
+    nb_tunas = nb_fishes * 0.6
+    nb_sharks = nb_fishes * 0.4
     shark_energy = 6
